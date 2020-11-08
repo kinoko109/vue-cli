81a2c69bda689fc7e53af09ebe400ea5a8dbc8f3
松岡龍紀 <tatsuaki_matsuoka@bita.jp>
2020-11-08 21:03:11 +0900

    update: counterをvuexで実装

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
     <p>{{ count }}</p>
+    <p>{{ doubleCounter }}</p>
+    <button @click="increment(1)">+1</button>
+    <button @click="decrement(2)">-1</button>
   </div>
 </template>
 
 <script>
+import {mapGetters, mapMutations, mapActions} from "vuex";
+
 export default {
   computed: {
     count() {
-      return this.$store.state.count;
-    }
+      return this.$store.state.count
+    },
+    ...mapGetters(["doubleCounter"])
+  },
+  methods: {
+    ...mapActions(["increment"]),
+    ...mapMutations(["decrement"])
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,23 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-        count: 0
+  state: {
+    count: 0
+  },
+  getters: {
+    doubleCounter: state => state.count * 2
+  },
+  mutations: {
+    increment(state, number) {
+      state.count += number;
+    },
+    decrement(state, number) {
+      state.count -= number;
     }
+  },
+  actions: {
+    increment({commit}, number) {
+      commit("increment", number);
+    }
+  }
 })
